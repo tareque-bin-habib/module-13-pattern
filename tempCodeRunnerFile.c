b3d091742ac43ce7974c01b37f1333d97be4df89
tareque-bin-habib <shad.tbh@gmail.com>
2023-04-30 00:18:20 +0600

    first commit

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[i] < a[j])
+            {
+                int tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+            }
+        }
+    }
